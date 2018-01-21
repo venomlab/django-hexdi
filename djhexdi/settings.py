5ec0aff1e36adba6be864b5312291c0e7034923a
dimonzo <zibertscrem@gmail.com>
2018-01-21 03:36:45 +0200

    Reformatted settings Refactored strategies

--- djhexdi/settings.py
+++ djhexdi/settings.py
@@ -2,7 +2,9 @@ from django.conf import settings
 
 HEXDI_STRATEGY = getattr(settings, 'HEXDI_STRATEGY', 'djhexdi.strategy.Dummy')
 HEXDI_FINDER_PACKAGES = getattr(settings, 'HEXDI_FINDER_PACKAGES', settings.INSTALLED_APPS)
-HEXDI_MODULES_LIST_NAME = getattr(settings, 'HEXDI_MODULE_LIST_NAME', 'HEXDI_MODULES_LIST')
-HEXDI_MODULES_LIST = getattr(settings, HEXDI_MODULES_LIST_NAME, None)
+HEXDI_MODULES_LIST = getattr(settings, 'HEXDI_MODULES_LIST', [])
 HEXDI_EXCLUDE_MODULES = getattr(settings, 'HEXDI_EXCLUDE_MODULES', [])
+HEXDI_MODULE_LIST_NAME = getattr(settings, 'HEXDI_MODULE_LIST_NAME', 'HEXDI_MODULES_LIST')
 HEXDI_MODULE = getattr(settings, 'HEXDI_MODULE', None)
+HEXDI_LOADER = getattr(settings, 'HEXDI_LOADER', 'hexdi.loader.BasicLoader')
+HEXDI_FINDER = getattr(settings, 'HEXDI_LOADER', 'hexdi.finder.RecursiveRegexFinder')
--- djhexdi/strategy.py
+++ djhexdi/strategy.py
@@ -1,35 +1,59 @@
-import hexdi
+import abc
+import typing
+from hexdi.utils import load_module, get_module_if_exists, load_class
 from djhexdi import settings
-from hexdi.utils import load_module, get_module_if_exists
 
 
-class AbstractStrategy:
+def load_modules(modules, excluded_modules):
+    loader = load_class(settings.HEXDI_LOADER)([m for m in modules if m not in excluded_modules])
+    loader.load()
+
+
+def find_modules(packages):
+    finder = load_class(settings.HEXDI_FINDER)(packages)
+    return finder.find()
+
+
+class AbstractStrategy(abc.ABC):
+    @abc.abstractmethod
+    def go(self): pass
+
+
+class Dummy(AbstractStrategy):
     def go(self):
         pass
 
 
-class Dummy(AbstractStrategy):
-    pass
+class AbstractLoadModulesStrategy(AbstractStrategy):
+    @abc.abstractmethod
+    def discover_modules(self) -> typing.List[typing.AnyStr]:
+        pass
 
-
-class Static(AbstractStrategy):
-    def _load_modules(self, modules):
-        loader = hexdi.get_loader([m for m in modules if m not in settings.HEXDI_EXCLUDE_MODULES])
-        loader.load()
+    def get_excluded_modules(self) -> typing.List[typing.AnyStr]:
+        return settings.HEXDI_EXCLUDE_MODULES
 
     def go(self):
+        modules = self.discover_modules()
+        excluded = self.get_excluded_modules()
+        load_modules(modules, excluded)
+
+
+class Static(AbstractLoadModulesStrategy):
+
+    def discover_modules(self) -> typing.List[typing.AnyStr]:
         modules = list()
-        modules.extend(settings.HEXDI_MODULES_LIST or [])
         file = settings.HEXDI_MODULE
         if file and get_module_if_exists(file):
             _module = load_module(file)
-            modules.extend(getattr(_module, settings.HEXDI_MODULES_LIST_NAME))
-        self._load_modules(modules)
+            modules.extend(getattr(_module, settings.HEXDI_MODULE_LIST_NAME))
+        modules.extend(settings.HEXDI_MODULES_LIST)
+        return modules
 
 
 class Auto(Static):
-    def go(self):
+    def discover_modules(self) -> typing.List[typing.AnyStr]:
         packages = settings.HEXDI_FINDER_PACKAGES
-        finder = hexdi.get_finder(packages)
-        modules = finder.find()
-        self._load_modules(modules)
+        modules = find_modules(packages)
+        static_modules = super(Auto, self).discover_modules()
+        modules.extend(static_modules)
+        return modules
